dcb66beb24eed70fc2c8ac759e6e9b973002f339
tripanboi-png <tripanboi@gmail.com>
2026-05-26 14:55:38 +0700

    Update addbutton.py

--- plugins/addbutton.py
+++ plugins/addbutton.py
@@ -1,25 +1,10 @@
-import json
-
 from pyrogram import Client, filters
-from pyrogram.types import Message
-
 from config import ADMINS
-
-BUTTON_FILE = "buttons.json"
-
-
-def load_buttons():
-    with open(BUTTON_FILE, "r") as f:
-        return json.load(f)
-
-
-def save_buttons(buttons):
-    with open(BUTTON_FILE, "w") as f:
-        json.dump(buttons, f)
+from database.mongo import add_button
 
 
 @Client.on_message(filters.command("addbutton") & filters.user(ADMINS))
-async def add_button(client, message: Message):
+async def add_button_command(client, message):
 
     if len(message.command) < 3:
         return await message.reply_text(
@@ -27,16 +12,9 @@ async def add_button(client, message: Message):
         )
 
     name = message.command[1]
-    link = message.command[2]
+    url = message.command[2]
 
-    buttons = load_buttons()
-
-    buttons.append({
-        "name": name,
-        "link": link
-    })
-
-    save_buttons(buttons)
+    await add_button(name, url)
 
     await message.reply_text(
         f"✅ Button berhasil ditambahkan\n\n{name}"
