7a62bb1c047c67be55220a47093941704962d02d
tripanboi-png <tripanboi@gmail.com>
2026-05-26 14:51:38 +0700

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,11 +1,28 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
 
-mongo_client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL)
 
-db = mongo_client["chindo_bot"]
+db = client["FileSharingBot"]
 
-admins_col = db.admins
-buttons_col = db.buttons
-users_col = db.users
-settings_col = db.settings
+admin_collection = db["admins"]
+
+
+async def add_admin(user_id):
+    user = await admin_collection.find_one({"user_id": user_id})
+
+    if not user:
+        await admin_collection.insert_one({"user_id": user_id})
+
+
+async def remove_admin(user_id):
+    await admin_collection.delete_one({"user_id": user_id})
+
+
+async def get_admins():
+    admins = []
+
+    async for admin in admin_collection.find():
+        admins.append(admin["user_id"])
+
+    return admins
