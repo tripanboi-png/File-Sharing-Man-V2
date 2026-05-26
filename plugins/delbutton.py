b21d4d154d20496acf0a6b3c846c52813f72fefc
tripanboi-png <tripanboi@gmail.com>
2026-05-26 14:57:25 +0700

    Update delbutton.py

--- plugins/delbutton.py
+++ plugins/delbutton.py
@@ -1,53 +1,20 @@
-import json
-
 from pyrogram import Client, filters
-from pyrogram.types import Message
-
 from config import ADMINS
-
-BUTTON_FILE = "buttons.json"
-
-
-def load_buttons():
-    with open(BUTTON_FILE, "r") as f:
-        return json.load(f)
-
-
-def save_buttons(buttons):
-    with open(BUTTON_FILE, "w") as f:
-        json.dump(buttons, f)
+from database.mongo import remove_button
 
 
 @Client.on_message(filters.command("delbutton") & filters.user(ADMINS))
-async def del_button(client, message: Message):
+async def del_button_command(client, message):
 
     if len(message.command) < 2:
         return await message.reply_text(
-            "Usage:\n/delbutton Nama"
+            "Usage:\n/delbutton NamaButton"
         )
 
     name = message.command[1]
 
-    buttons = load_buttons()
+    await remove_button(name)
 
-    new_buttons = []
-
-    deleted = False
-
-    for x in buttons:
-
-        if x["name"] != name:
-            new_buttons.append(x)
-        else:
-            deleted = True
-
-    save_buttons(new_buttons)
-
-    if deleted:
-        await message.reply_text(
-            f"❌ Button {name} berhasil dihapus"
-        )
-    else:
-        await message.reply_text(
-            "Button tidak ditemukan."
-        )
+    await message.reply_text(
+        f"❌ Button berhasil dihapus\n\n{name}"
+    )
