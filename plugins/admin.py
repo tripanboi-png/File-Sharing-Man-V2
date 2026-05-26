3c19f1e83291841bd705480cdf3ac6cf63864eab
tripanboi-png <tripanboi@gmail.com>
2026-05-26 17:24:04 +0700

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,43 +1,25 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.check_admin import is_admin
+from database.mongo import add_admin
 
 
-@Client.on_message(filters.command("admin") & filters.create(is_admin))
-async def admin_menu(client, message):
+@Client.on_message(filters.command("addadmin") & filters.create(is_admin))
+async def add_admin_command(client, message):
 
-    text = f"""
-<b>⚡ MENU ADMIN ⚡</b>
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "Usage:\n/addadmin user_id"
+        )
 
-<code>/users</code> - cek total users
-<code>/broadcast</code> - broadcast pesan
-<code>/ping</code> - cek speed bot
-<code>/uptime</code> - cek uptime
+    try:
+        user_id = int(message.command[1])
+    except:
+        return await message.reply_text(
+            "User ID tidak valid"
+        )
 
-<code>/batch</code> - buat link banyak file
-<code>/genlink</code> - buat link 1 file
-
-<code>/logs</code> - lihat logs
-<code>/restart</code> - restart bot
-"""
-
-    btn = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🔥 CHANNEL", url="https://t.me/USERNAMECHANNEL"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "❌ TUTUP", callback_data="close"
-                )
-            ]
-        ]
-    )
+    await add_admin(user_id)
 
     await message.reply_text(
-        text=text,
-        reply_markup=btn,
-        disable_web_page_preview=True
+        f"✅ Admin berhasil ditambahkan\n\nID: <code>{user_id}</code>"
     )
