5d5f5ae4baf29bc1d0c7a8342e9deface0361429
tripanboi-png <tripanboi@gmail.com>
2026-05-26 14:19:23 +0700

    Update getadmin.py

--- plugins/getadmin.py
+++ plugins/getadmin.py
@@ -1,10 +1,25 @@
+import json
+
 from pyrogram import Client, filters
 from config import ADMINS
 
-admins = ADMINS.copy()
+ADMIN_FILE = "admins.json"
+
+
+def load_admins():
+    with open(ADMIN_FILE, "r") as f:
+        return json.load(f)
+
 
 @Client.on_message(filters.command("getadmin") & filters.user(ADMINS))
 async def get_admin(client, message):
+
+    admins = load_admins()
+
+    if not admins:
+        return await message.reply_text(
+            "Belum ada admin tambahan."
+        )
 
     text = "📋 LIST ADMIN\n\n"
 
