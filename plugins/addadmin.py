8486437b6e1a399015ea7c7ad7a1e3e8fc049c00
tripanboi-png <tripanboi@gmail.com>
2026-05-26 14:07:33 +0700

    Update addadmin.py

--- plugins/addadmin.py
+++ plugins/addadmin.py
@@ -1,18 +1,24 @@
 from pyrogram import Client, filters
-
+from pyrogram.types import Message
 from config import ADMINS
 
+admins = ADMINS.copy()
 
 @Client.on_message(filters.command("addadmin") & filters.user(ADMINS))
-async def add_admin(client, message):
+async def add_admin(client, message: Message):
 
     if len(message.command) < 2:
         return await message.reply_text(
             "Usage:\n/addadmin user_id"
         )
 
-    user_id = message.command[1]
+    user_id = int(message.command[1])
+
+    if user_id in admins:
+        return await message.reply_text("Admin sudah ada.")
+
+    admins.append(user_id)
 
     await message.reply_text(
-        f"✅ Admin berhasil ditambahkan:\n<code>{user_id}</code>"
+        f"✅ Admin berhasil ditambahkan\n\nID: <code>{user_id}</code>"
     )
