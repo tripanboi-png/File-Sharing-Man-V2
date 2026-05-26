2aca28abee4e4c8405526525039c0c170fb4d153
tripanboi-png <tripanboi@gmail.com>
2026-05-26 17:19:22 +0700

    Update addbutton.py

--- plugins/addbutton.py
+++ plugins/addbutton.py
@@ -3,26 +3,19 @@ from plugins.check_admin import is_admin
 from database.mongo import add_button
 
 
-@Client.on_message(filters.command("addbutton") & filters.create(is_admin)
+@Client.on_message(filters.command("addbutton") & filters.create(is_admin))
 async def add_button_command(client, message):
 
-    parts = message.text.split(maxsplit=2)
-
-    if len(parts) < 3:
+    if len(message.command) < 3:
         return await message.reply_text(
             "Usage:\n/addbutton Nama https://t.me/link"
         )
 
-    name = parts[1]
-    url = parts[2]
-
-    if not url.startswith("https://t.me/"):
-        return await message.reply_text(
-            "❌ Link harus format:\nhttps://t.me/username"
-        )
+    name = message.command[1]
+    url = message.command[2]
 
     await add_button(name, url)
 
     await message.reply_text(
-        f"✅ Button berhasil ditambahkan\n\n📌 Nama: {name}\n🔗 URL: {url}"
+        f"✅ Button berhasil ditambahkan\n\n{name}"
     )
