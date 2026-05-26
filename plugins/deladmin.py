2a72bbf38ed5e7b4e91c1aa15faf64120ac4fb89
tripanboi-png <tripanboi@gmail.com>
2026-05-26 14:52:59 +0700

    Update deladmin.py

--- plugins/deladmin.py
+++ plugins/deladmin.py
@@ -1,24 +1,10 @@
-import json
-
 from pyrogram import Client, filters
-from pyrogram.types import Message
 from config import ADMINS
-
-ADMIN_FILE = "admins.json"
-
-
-def load_admins():
-    with open(ADMIN_FILE, "r") as f:
-        return json.load(f)
-
-
-def save_admins(admins):
-    with open(ADMIN_FILE, "w") as f:
-        json.dump(admins, f)
+from database.mongo import remove_admin
 
 
 @Client.on_message(filters.command("deladmin") & filters.user(ADMINS))
-async def del_admin(client, message: Message):
+async def del_admin_command(client, message):
 
     if len(message.command) < 2:
         return await message.reply_text(
@@ -27,15 +13,10 @@ async def del_admin(client, message: Message):
 
     user_id = int(message.command[1])
 
-    admins = load_admins()
+    if user_id in ADMINS:
+        ADMINS.remove(user_id)
 
-    if user_id not in admins:
-        return await message.reply_text(
-            "User bukan admin."
-        )
-
-    admins.remove(user_id)
-    save_admins(admins)
+    await remove_admin(user_id)
 
     await message.reply_text(
         f"❌ Admin berhasil dihapus\n\nID: <code>{user_id}</code>"
