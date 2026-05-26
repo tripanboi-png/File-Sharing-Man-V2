22adeb2f560de6e111a2066876a3ee54f8f96ab9
tripanboi-png <tripanboi@gmail.com>
2026-05-26 14:55:00 +0700

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -5,24 +5,78 @@ client = AsyncIOMotorClient(MONGO_URL)
 
 db = client["FileSharingBot"]
 
+# ================= ADMIN =================
+
 admin_collection = db["admins"]
 
 
 async def add_admin(user_id):
-    user = await admin_collection.find_one({"user_id": user_id})
+
+    user = await admin_collection.find_one(
+        {
+            "user_id": user_id
+        }
+    )
 
     if not user:
-        await admin_collection.insert_one({"user_id": user_id})
+
+        await admin_collection.insert_one(
+            {
+                "user_id": user_id
+            }
+        )
 
 
 async def remove_admin(user_id):
-    await admin_collection.delete_one({"user_id": user_id})
+
+    await admin_collection.delete_one(
+        {
+            "user_id": user_id
+        }
+    )
 
 
 async def get_admins():
+
     admins = []
 
     async for admin in admin_collection.find():
+
         admins.append(admin["user_id"])
 
     return admins
+
+
+# ================= BUTTON =================
+
+button_collection = db["buttons"]
+
+
+async def add_button(name, url):
+
+    await button_collection.insert_one(
+        {
+            "name": name,
+            "url": url
+        }
+    )
+
+
+async def remove_button(name):
+
+    await button_collection.delete_one(
+        {
+            "name": name
+        }
+    )
+
+
+async def get_buttons():
+
+    buttons = []
+
+    async for button in button_collection.find():
+
+        buttons.append(button)
+
+    return buttons
