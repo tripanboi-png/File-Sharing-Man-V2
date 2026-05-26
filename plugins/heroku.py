f9a1d7d5b350dcca56a2ff2f8d6b139bceaaba57
tripanboi-png <tripanboi@gmail.com>
2026-05-26 09:50:56 +0700

    Update heroku.py

--- plugins/heroku.py
+++ plugins/heroku.py
@@ -1,51 +1,23 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-# Ported by @mrismanaziz
-# FROM File-Sharing-Man < https://github.com/mrismanaziz/File-Sharing-Man/ >
-# t.me/Lunatic0de & t.me/SharingUserbot
-#
+# Heroku Config Manager Safe Version
 
 import os
 import socket
-
 import dotenv
 import heroku3
 import urllib3
+
 from bot import Bot
 from config import ADMINS, HEROKU_API_KEY, HEROKU_APP_NAME
 from pyrogram import filters
 from pyrogram.types import Message
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
+
+if HEROKU_APP_NAME and HEROKU_API_KEY:
     Heroku = heroku3.from_key(HEROKU_API_KEY)
     HAPP = Heroku.app(HEROKU_APP_NAME)
-    heroku_config = HAPP.config()
 else:
     HAPP = None
-
-XCB = [
-    "/",
-    "@",
-    ".",
-    "com",
-    ":",
-    "git",
-    "heroku",
-    "push",
-    str(HEROKU_API_KEY),
-    "https",
-    str(HEROKU_APP_NAME),
-    "HEAD",
-    "main",
-]
 
 
 async def is_heroku():
@@ -53,91 +25,118 @@ async def is_heroku():
 
 
 @Bot.on_message(filters.command("getvar") & filters.user(ADMINS))
-async def varget_(client: Bot, message: Message):
+async def get_var(client: Bot, message: Message):
     if len(message.command) != 2:
-        return await message.reply_text("<b>Usage:</b>\n/getvar [Var Name]")
-    check_var = message.text.split(None, 2)[1]
+        return await message.reply_text(
+            "<b>Usage:</b>\n/getvar [VAR_NAME]"
+        )
+
+    var_name = message.command[1]
+
     if await is_heroku():
         if HAPP is None:
             return await message.reply_text(
-                "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
-            )
-        heroku_config = HAPP.config()
-        if check_var in heroku_config:
-            return await message.reply_text(
-                f"<b>{check_var}:</b> <code>{heroku_config[check_var]}</code>"
-            )
-        else:
-            return await message.reply_text(f"Tidak dapat menemukan var {check_var}")
-    else:
-        path = dotenv.find_dotenv("config.env")
-        if not path:
-            return await message.reply_text(".env file not found.")
-        output = dotenv.get_key(path, check_var)
-        if not output:
-            await message.reply_text(f"Tidak dapat menemukan var {check_var}")
-        else:
-            return await message.reply_text(
-                f"<b>{check_var}:</b> <code>{str(output)}</code>"
+                "HEROKU_API_KEY atau HEROKU_APP_NAME belum diatur."
             )
 
+        config = HAPP.config()
 
-@Bot.on_message(filters.command("delvar") & filters.user(ADMINS))
-async def vardel_(client: Bot, message: Message):
-    if len(message.command) != 2:
-        return await message.reply_text("<b>Usage:</b>\n/delvar [Var Name]")
-    check_var = message.text.split(None, 2)[1]
-    if await is_heroku():
-        if HAPP is None:
+        if var_name in config:
             return await message.reply_text(
-                "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
+                f"<b>{var_name}:</b>\n<code>{config[var_name]}</code>"
             )
-        heroku_config = HAPP.config()
-        if check_var in heroku_config:
-            await message.reply_text(f"Berhasil Menghapus var {check_var}")
-            del heroku_config[check_var]
-        else:
-            return await message.reply_text(f"Tidak dapat menemukan var {check_var}")
+
+        return await message.reply_text("Variable tidak ditemukan.")
+
     else:
         path = dotenv.find_dotenv("config.env")
+
         if not path:
-            return await message.reply_text(".env file not found.")
-        output = dotenv.unset_key(path, check_var)
-        if not output[0]:
-            return await message.reply_text(f"Tidak dapat menemukan var {check_var}")
+            return await message.reply_text("config.env tidak ditemukan.")
+
+        value = dotenv.get_key(path, var_name)
+
+        if value:
+            await message.reply_text(
+                f"<b>{var_name}:</b>\n<code>{value}</code>"
+            )
         else:
-            await message.reply_text(f"Berhasil Menghapus var {check_var}")
-            os.system(f"kill -9 {os.getpid()} && bash start")
+            await message.reply_text("Variable tidak ditemukan.")
 
 
 @Bot.on_message(filters.command("setvar") & filters.user(ADMINS))
 async def set_var(client: Bot, message: Message):
     if len(message.command) < 3:
-        return await message.reply_text("<b>Usage:</b>\n/setvar [Var Name] [Var Value]")
-    to_set = message.text.split(None, 2)[1].strip()
-    value = message.text.split(None, 2)[2].strip()
+        return await message.reply_text(
+            "<b>Usage:</b>\n/setvar [VAR_NAME] [VALUE]"
+        )
+
+    var_name = message.command[1]
+    value = message.text.split(None, 2)[2]
+
     if await is_heroku():
         if HAPP is None:
             return await message.reply_text(
-                "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
+                "HEROKU_API_KEY atau HEROKU_APP_NAME belum diatur."
             )
-        heroku_config = HAPP.config()
-        if to_set in heroku_config:
-            await message.reply_text(f"Berhasil Mengubah var {to_set} menjadi {value}")
-        else:
-            await message.reply_text(
-                f"Berhasil Menambahkan var {to_set} menjadi {value}"
-            )
-        heroku_config[to_set] = value
+
+        config = HAPP.config()
+        config[var_name] = value
+
+        await message.reply_text(
+            f"Berhasil mengatur:\n<b>{var_name}</b>"
+        )
+
     else:
         path = dotenv.find_dotenv("config.env")
+
         if not path:
-            return await message.reply_text(".env file not found.")
-        dotenv.set_key(path, to_set, value)
-        if dotenv.get_key(path, to_set):
-            await message.reply_text(f"Berhasil Mengubah var {to_set} menjadi {value}")
-        else:
-            await message.reply_text(
-                f"Berhasil Menambahkan var {to_set} menjadi {value}"
+            return await message.reply_text("config.env tidak ditemukan.")
+
+        dotenv.set_key(path, var_name, value)
+
+        await message.reply_text(
+            f"Berhasil mengatur:\n<b>{var_name}</b>"
+        )
+
+        os.system(f"kill -9 {os.getpid()} && bash start")
+
+
+@Bot.on_message(filters.command("delvar") & filters.user(ADMINS))
+async def del_var(client: Bot, message: Message):
+    if len(message.command) != 2:
+        return await message.reply_text(
+            "<b>Usage:</b>\n/delvar [VAR_NAME]"
+        )
+
+    var_name = message.command[1]
+
+    if await is_heroku():
+        if HAPP is None:
+            return await message.reply_text(
+                "HEROKU_API_KEY atau HEROKU_APP_NAME belum diatur."
             )
+
+        config = HAPP.config()
+
+        if var_name in config:
+            del config[var_name]
+            return await message.reply_text(
+                f"Berhasil menghapus:\n<b>{var_name}</b>"
+            )
+
+        return await message.reply_text("Variable tidak ditemukan.")
+
+    else:
+        path = dotenv.find_dotenv("config.env")
+
+        if not path:
+            return await message.reply_text("config.env tidak ditemukan.")
+
+        dotenv.unset_key(path, var_name)
+
+        await message.reply_text(
+            f"Berhasil menghapus:\n<b>{var_name}</b>"
+        )
+
         os.system(f"kill -9 {os.getpid()} && bash start")
