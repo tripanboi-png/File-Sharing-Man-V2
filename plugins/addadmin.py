d3636b352e5d34a9a30c95d22dcae1b332c1e9c3
tripanboi-png <tripanboi@gmail.com>
2026-05-30 13:55:06 +0700

    Update addadmin.py

--- plugins/addadmin.py
+++ plugins/addadmin.py
@@ -1,20 +1,5 @@
 from pyrogram import Client, filters
-from plugins.check_admin import is_admin
-from database.mongo import add_admin
 
-
-@Client.on_message(filters.command("addadmin") & filters.create(is_admin))
+@Client.on_message(filters.command("addadmin"))
 async def add_admin_command(client, message):
-
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "Usage:\n/addadmin user_id"
-        )
-
-    user_id = int(message.command[1])
-
-    await add_admin(user_id)
-
-    await message.reply_text(
-        f"✅ Admin berhasil ditambahkan\n\nID: <code>{user_id}</code>"
-    )
+    await message.reply_text("ADDADMIN TERPANGGIL")
