5691e6f62f2833dcaf639fd6d249b40034da7221
tripanboi-png <tripanboi@gmail.com>
2026-05-30 13:57:03 +0700

    Update check_admin.py

--- plugins/check_admin.py
+++ plugins/check_admin.py
@@ -1,10 +1,13 @@
 from database.mongo import get_admins
 
 async def is_admin(_, __, message):
-    user = getattr(message, "from_user", None)
 
-    if user is None:
+    if not message.from_user:
         return False
 
     admins = await get_admins()
-    return user.id in admins
+
+    print("USER ID:", message.from_user.id)
+    print("ADMINS :", admins)
+
+    return message.from_user.id in admins
